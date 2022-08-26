c86763d89fba7d1e40b6c30d4048a838ceb3b133
hamza.yahaya <hamza.yahaya@dynatrace.com>
2022-08-26 15:30:33 +0200

    upgrade to version 1.8.1

--- monaco/tools/chocolateyinstall.ps1
+++ monaco/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 # Do NOT CHANGE THESE MANUALLY. USE update.ps1
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/v1.8.0/monaco-windows-4.0-386.exe'
-$url64       = 'https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/v1.8.0/monaco-windows-4.0-amd64.exe'
+$url         = 'https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/v1.8.1/monaco-windows-4.0-386.exe'
+$url64       = 'https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/v1.8.1/monaco-windows-4.0-amd64.exe'
 $checksum    = '0858B7E1BC50623D8743A320F5C6F155B6E00BC43EB8A0C3E46327F82CA09D80'
 $checksum64  = '1438AEA7B2CC1F25FEFB776850278A09CE115A63B0910356F5CE3DE896A3AC64'
 $PackageName = 'monaco.exe'
